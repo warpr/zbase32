65d6e547d1cd8fa56675dd12ebc9f3ffba150bc4
kuno <kuno@frob.nl>
2019-07-04 08:10:08 -0500

    Remove eslint rules, include .prettierrc

--- index.js
+++ index.js
@@ -6,140 +6,138 @@
  *   it under the terms of copyleft-next 0.3.1.  See copyleft-next-0.3.1.txt.
  */
 
-"use strict";
+'use strict';
 
 (function(factory) {
-  const imports = ["require"];
+    const imports = ['require'];
 
-  if (typeof define === "function" && define.amd) {
-    define(imports, factory);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = factory(require);
-  } else {
-    console.log("Module system not recognized, please use AMD or CommonJS");
-  }
+    if (typeof define === 'function' && define.amd) {
+        define(imports, factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require);
+    } else {
+        console.log('Module system not recognized, please use AMD or CommonJS');
+    }
 })(function(require) {
-  const MNET32 = "ybndrfg8ejkmcpqxot1uwisza345h769";
-  const MNET32Reverse = {};
-  for (let i = 0; i < MNET32.length; i++) {
-    MNET32Reverse[MNET32[i]] = i;
-  }
+    const MNET32 = 'ybndrfg8ejkmcpqxot1uwisza345h769';
+    const MNET32Reverse = {};
+    for (let i = 0; i < MNET32.length; i++) {
+        MNET32Reverse[MNET32[i]] = i;
+    }
 
-  function IntegerTooLargeException(message) {
-    this.message = message;
-    this.name = "IntegerTooLargeException";
-  }
+    function IntegerTooLargeException(message) {
+        this.message = message;
+        this.name = 'IntegerTooLargeException';
+    }
 
-  function getBit(byte, offset) {
-    return (byte >> offset) & 0x01;
-  }
+    function getBit(byte, offset) {
+        return (byte >> offset) & 0x01;
+    }
 
-  function to5bit(ab) {
-    const bytes = new Uint8Array(ab);
+    function to5bit(ab) {
+        const bytes = new Uint8Array(ab);
 
-    let bitCount = 0;
-    let chunks = [];
-    let currentChunk = 0;
-    for (let i = bytes.length - 1; i >= 0; i--) {
-      for (let j = 0; j < 8; j++) {
-        currentChunk = (currentChunk >> 1) | (getBit(bytes[i], j) << 4);
-        if (++bitCount > 4) {
-          bitCount = 0;
-          chunks.push(currentChunk);
-          currentChunk = 0;
+        let bitCount = 0;
+        let chunks = [];
+        let currentChunk = 0;
+        for (let i = bytes.length - 1; i >= 0; i--) {
+            for (let j = 0; j < 8; j++) {
+                currentChunk = (currentChunk >> 1) | (getBit(bytes[i], j) << 4);
+                if (++bitCount > 4) {
+                    bitCount = 0;
+                    chunks.push(currentChunk);
+                    currentChunk = 0;
+                }
+            }
         }
-      }
-    }
 
-    if (bitCount) {
-      chunks.push(currentChunk >> (5 - bitCount));
-    }
-
-    chunks.reverse();
-    return chunks;
-  }
-
-  function from5bit(ab) {
-    const chunks = new Uint8Array(ab);
-
-    let bitCount = 0;
-    let bytes = [];
-    let currentByte = 0;
-    for (let i = chunks.length - 1; i >= 0; i--) {
-      for (let j = 0; j < 5; j++) {
-        currentByte = (currentByte >> 1) | (getBit(chunks[i], j) << 7);
-        if (++bitCount > 7) {
-          bitCount = 0;
-          bytes.push(currentByte);
-          currentByte = 0;
+        if (bitCount) {
+            chunks.push(currentChunk >> (5 - bitCount));
         }
-      }
+
+        chunks.reverse();
+        return chunks;
     }
 
-    bytes.reverse();
-    return new Uint8Array(bytes);
-  }
+    function from5bit(ab) {
+        const chunks = new Uint8Array(ab);
 
-  function toNumber(ab) {
-    return (
-      (ab[0] << 30) |
-      (ab[1] << 25) |
-      (ab[2] << 20) |
-      (ab[3] << 15) |
-      (ab[4] << 10) |
-      (ab[5] << 5) |
-      ab[6]
-    );
-  }
+        let bitCount = 0;
+        let bytes = [];
+        let currentByte = 0;
+        for (let i = chunks.length - 1; i >= 0; i--) {
+            for (let j = 0; j < 5; j++) {
+                currentByte = (currentByte >> 1) | (getBit(chunks[i], j) << 7);
+                if (++bitCount > 7) {
+                    bitCount = 0;
+                    bytes.push(currentByte);
+                    currentByte = 0;
+                }
+            }
+        }
 
-  function fromNumber(num) {
-    if (num < 0 || num > 0x7fffffff) {
-      throw new IntegerTooLargeException(
-        "fromNumber expects a 32-bit signed integer"
-      );
+        bytes.reverse();
+        return new Uint8Array(bytes);
     }
 
-    // Although javascript Numbers can safely represent integers up to (2^53)-1, the
-    // bitwise operators operate on them as 32-bit signed integers.
-    // FIXME: look for a popular bigint library which can easily convert big integers as
-    // ArrayBuffers.
-    return new Uint8Array([
-      (num >>> 30) & 0x03,
-      (num >>> 25) & 0x1f,
-      (num >>> 20) & 0x1f,
-      (num >>> 15) & 0x1f,
-      (num >>> 10) & 0x1f,
-      (num >>> 5) & 0x1f,
-      (num >>> 0) & 0x1f
-    ]);
-  }
+    function toNumber(ab) {
+        return (
+            (ab[0] << 30) |
+            (ab[1] << 25) |
+            (ab[2] << 20) |
+            (ab[3] << 15) |
+            (ab[4] << 10) |
+            (ab[5] << 5) |
+            ab[6]
+        );
+    }
 
-  function decode32bitNumber(str) {
-    return toNumber(decode(str));
-  }
+    function fromNumber(num) {
+        if (num < 0 || num > 0x7fffffff) {
+            throw new IntegerTooLargeException('fromNumber expects a 32-bit signed integer');
+        }
 
-  function encode32bitNumber(int) {
-    return encode(fromNumber(int));
-  }
+        // Although javascript Numbers can safely represent integers up to (2^53)-1, the
+        // bitwise operators operate on them as 32-bit signed integers.
+        // FIXME: look for a popular bigint library which can easily convert big integers as
+        // ArrayBuffers.
+        return new Uint8Array([
+            (num >>> 30) & 0x03,
+            (num >>> 25) & 0x1f,
+            (num >>> 20) & 0x1f,
+            (num >>> 15) & 0x1f,
+            (num >>> 10) & 0x1f,
+            (num >>> 5) & 0x1f,
+            (num >>> 0) & 0x1f,
+        ]);
+    }
 
-  function decode(x) {
-    return from5bit(x.split("").map(chr => MNET32Reverse[chr]));
-  }
+    function decode32bitNumber(str) {
+        return toNumber(decode(str));
+    }
 
-  function encode(x) {
-    return to5bit(x)
-      .map(value => MNET32[value])
-      .join("");
-  }
+    function encode32bitNumber(int) {
+        return encode(fromNumber(int));
+    }
 
-  return {
-    decode: decode,
-    encode: encode,
-    from5bit: from5bit,
-    to5bit: to5bit,
-    fromNumber: fromNumber,
-    toNumber: toNumber,
-    decode32bitNumber: decode32bitNumber,
-    encode32bitNumber: encode32bitNumber
-  };
+    function decode(x) {
+        return from5bit(x.split('').map(chr => MNET32Reverse[chr]));
+    }
+
+    function encode(x) {
+        return to5bit(x)
+            .map(value => MNET32[value])
+            .join('');
+    }
+
+    return {
+        decode: decode,
+        encode: encode,
+        from5bit: from5bit,
+        to5bit: to5bit,
+        fromNumber: fromNumber,
+        toNumber: toNumber,
+        decode32bitNumber: decode32bitNumber,
+        encode32bitNumber: encode32bitNumber,
+    };
 });
--- test/test.js
+++ test/test.js
@@ -6,144 +6,144 @@
  *   it under the terms of copyleft-next 0.3.1.  See copyleft-next-0.3.1.txt.
  */
 
-"use strict";
+'use strict';
 
 (function(factory) {
-  const imports = ["require", "chai", "text-encoding", "jsverify", "../index"];
+    const imports = ['require', 'chai', 'text-encoding', 'jsverify', '../index'];
 
-  if (typeof define === "function" && define.amd) {
-    define(imports, factory);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = factory(require);
-  } else {
-    console.log("Module system not recognized, please use AMD or CommonJS");
-  }
+    if (typeof define === 'function' && define.amd) {
+        define(imports, factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require);
+    } else {
+        console.log('Module system not recognized, please use AMD or CommonJS');
+    }
 })(function(require) {
-  const assert = require("chai").assert;
-  const zbase32 = require("../index");
-  const TextDecoder = require("text-encoding").TextDecoder;
-  const TextEncoder = require("text-encoding").TextEncoder;
-  const jsverify = require("jsverify");
+    const assert = require('chai').assert;
+    const zbase32 = require('../index');
+    const TextDecoder = require('text-encoding').TextDecoder;
+    const TextEncoder = require('text-encoding').TextEncoder;
+    const jsverify = require('jsverify');
 
-  function toUTF8(str) {
-    return new TextEncoder("utf-8").encode(str);
-  }
+    function toUTF8(str) {
+        return new TextEncoder('utf-8').encode(str);
+    }
 
-  function fromUTF8(arrayBuffer) {
-    return new TextDecoder("utf-8").decode(arrayBuffer);
-  }
+    function fromUTF8(arrayBuffer) {
+        return new TextDecoder('utf-8').decode(arrayBuffer);
+    }
 
-  suite("zbase32", function() {
-    test("to5bit", function() {
-      //  H 0x48   e 0x65   l 0x6c   l 0x6c   o 0x6f
-      // 01001000 01100101 01101100 01101100 01101111
-      //
-      //  0x09  0x01  0x12  0x16  0x18  0x1b  0x03  0x0f
-      // 01001 00001 10010 10110 11000 11011 00011 01111
-      let expected = [0x09, 0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
-      assert.deepEqual(zbase32.to5bit(toUTF8("Hello")), expected);
+    suite('zbase32', function() {
+        test('to5bit', function() {
+            //  H 0x48   e 0x65   l 0x6c   l 0x6c   o 0x6f
+            // 01001000 01100101 01101100 01101100 01101111
+            //
+            //  0x09  0x01  0x12  0x16  0x18  0x1b  0x03  0x0f
+            // 01001 00001 10010 10110 11000 11011 00011 01111
+            let expected = [0x09, 0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
+            assert.deepEqual(zbase32.to5bit(toUTF8('Hello')), expected);
 
-      expected = [0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
-      assert.deepEqual(zbase32.to5bit(toUTF8("ello")), expected);
+            expected = [0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
+            assert.deepEqual(zbase32.to5bit(toUTF8('ello')), expected);
 
-      expected = [0x06, 0x18, 0x1b, 0x03, 0x0f];
-      assert.deepEqual(zbase32.to5bit(toUTF8("llo")), expected);
+            expected = [0x06, 0x18, 0x1b, 0x03, 0x0f];
+            assert.deepEqual(zbase32.to5bit(toUTF8('llo')), expected);
 
-      expected = [0x0, 0x1b, 0x03, 0x0f];
-      assert.deepEqual(zbase32.to5bit(toUTF8("lo")), expected);
+            expected = [0x0, 0x1b, 0x03, 0x0f];
+            assert.deepEqual(zbase32.to5bit(toUTF8('lo')), expected);
 
-      expected = [0x03, 0x0f];
-      assert.deepEqual(zbase32.to5bit(toUTF8("o")), expected);
+            expected = [0x03, 0x0f];
+            assert.deepEqual(zbase32.to5bit(toUTF8('o')), expected);
+        });
+
+        test('from5bit', function() {
+            let input = [0x09, 0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
+            assert.equal(fromUTF8(zbase32.from5bit(input)), 'Hello');
+
+            input = [0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
+            assert.equal(fromUTF8(zbase32.from5bit(input)), 'ello');
+
+            input = [0x06, 0x18, 0x1b, 0x03, 0x0f];
+            assert.equal(fromUTF8(zbase32.from5bit(input)), 'llo');
+
+            input = [0x0, 0x1b, 0x03, 0x0f];
+            assert.equal(fromUTF8(zbase32.from5bit(input)), 'lo');
+
+            input = [0x03, 0x0f];
+            assert.equal(fromUTF8(zbase32.from5bit(input)), 'o');
+        });
+
+        test('encoding', function() {
+            assert.equal(zbase32.encode(toUTF8('hello')), 'pb1sa5dx');
+        });
+
+        test('decoding', function() {
+            assert.equal(fromUTF8(zbase32.decode('pb1sa5dx')), 'hello');
+        });
+
+        test('roundtrip', function() {
+            const roundTrip = jsverify.forall('string', testString => {
+                const encoded = zbase32.encode(toUTF8(testString));
+                return testString === fromUTF8(zbase32.decode(encoded));
+            });
+
+            // FIXME: size is the max size of each generated random value,
+            // it doesn't increase the test string size significantly.
+            // See also:
+            // - https://github.com/jsverify/jsverify/issues/167
+            // - https://github.com/jsverify/jsverify/issues/169
+            jsverify.assert(roundTrip, { size: Number.MAX_SAFE_INTEGER });
+        });
+
+        test('valid zbase32', function() {
+            const valid = jsverify.forall('string', testString => {
+                const encoded = zbase32.encode(toUTF8(testString));
+                return /^[13456789abcdefghijkmnopqrstuwxyz]*$/.test(encoded);
+            });
+
+            jsverify.assert(valid, { size: Number.MAX_SAFE_INTEGER });
+        });
+
+        test('fromNumber', function() {
+            // 11 00000 11111 00000 11111 00000 11111
+            const expected = [1, 0, 0x1f, 0, 0x1f, 0, 0x1f];
+            const buf = zbase32.fromNumber(0x41f07c1f);
+            assert.deepEqual(expected, [].slice.call(buf));
+        });
+
+        test('toNumber', function() {
+            const num = zbase32.toNumber([1, 0, 0x1f, 0, 0x1f, 0, 0x1f]);
+            assert.equal(0x41f07c1f, num);
+        });
+
+        test('encode number', function() {
+            assert.equal(zbase32.encode32bitNumber(0), 'yyyyyyyyyyyy');
+            assert.equal(zbase32.encode32bitNumber(23), 'yyyyyyyyyyyz');
+            assert.equal(zbase32.encode32bitNumber(0x7fffffff), 'yye9dhxt68a9');
+            assert.equal(zbase32.encode32bitNumber(0xffff), 'yyyyyyyt68a9');
+        });
+
+        test('decode number', function() {
+            assert.equal(zbase32.decode32bitNumber('yyyyyyyyyyyy'), 0);
+            assert.equal(zbase32.decode32bitNumber('yyyyyyyyyyyz'), 23);
+            assert.equal(zbase32.decode32bitNumber('yye9dhxt68a9'), 0x7fffffff);
+            assert.equal(zbase32.decode32bitNumber('yyyyyyyt68a9'), 0xffff);
+        });
+
+        test('number roundtrip', function() {
+            const roundTrip = jsverify.forall('string', testString => {
+                const encoded = zbase32.encode(toUTF8(testString));
+                return testString === fromUTF8(zbase32.decode(encoded));
+            });
+
+            // FIXME: size is the max size of each generated random value,
+            // it doesn't increase the test string size significantly.
+            // See also:
+            // - https://github.com/jsverify/jsverify/issues/167
+            // - https://github.com/jsverify/jsverify/issues/169
+            jsverify.assert(roundTrip, { size: Number.MAX_SAFE_INTEGER });
+        });
     });
-
-    test("from5bit", function() {
-      let input = [0x09, 0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
-      assert.equal(fromUTF8(zbase32.from5bit(input)), "Hello");
-
-      input = [0x01, 0x12, 0x16, 0x18, 0x1b, 0x03, 0x0f];
-      assert.equal(fromUTF8(zbase32.from5bit(input)), "ello");
-
-      input = [0x06, 0x18, 0x1b, 0x03, 0x0f];
-      assert.equal(fromUTF8(zbase32.from5bit(input)), "llo");
-
-      input = [0x0, 0x1b, 0x03, 0x0f];
-      assert.equal(fromUTF8(zbase32.from5bit(input)), "lo");
-
-      input = [0x03, 0x0f];
-      assert.equal(fromUTF8(zbase32.from5bit(input)), "o");
-    });
-
-    test("encoding", function() {
-      assert.equal(zbase32.encode(toUTF8("hello")), "pb1sa5dx");
-    });
-
-    test("decoding", function() {
-      assert.equal(fromUTF8(zbase32.decode("pb1sa5dx")), "hello");
-    });
-
-    test("roundtrip", function() {
-      const roundTrip = jsverify.forall("string", testString => {
-        const encoded = zbase32.encode(toUTF8(testString));
-        return testString === fromUTF8(zbase32.decode(encoded));
-      });
-
-      // FIXME: size is the max size of each generated random value,
-      // it doesn't increase the test string size significantly.
-      // See also:
-      // - https://github.com/jsverify/jsverify/issues/167
-      // - https://github.com/jsverify/jsverify/issues/169
-      jsverify.assert(roundTrip, { size: Number.MAX_SAFE_INTEGER });
-    });
-
-    test("valid zbase32", function() {
-      const valid = jsverify.forall("string", testString => {
-        const encoded = zbase32.encode(toUTF8(testString));
-        return /^[13456789abcdefghijkmnopqrstuwxyz]*$/.test(encoded);
-      });
-
-      jsverify.assert(valid, { size: Number.MAX_SAFE_INTEGER });
-    });
-
-    test("fromNumber", function() {
-      // 11 00000 11111 00000 11111 00000 11111
-      const expected = [1, 0, 0x1f, 0, 0x1f, 0, 0x1f];
-      const buf = zbase32.fromNumber(0x41f07c1f);
-      assert.deepEqual(expected, [].slice.call(buf));
-    });
-
-    test("toNumber", function() {
-      const num = zbase32.toNumber([1, 0, 0x1f, 0, 0x1f, 0, 0x1f]);
-      assert.equal(0x41f07c1f, num);
-    });
-
-    test("encode number", function() {
-      assert.equal(zbase32.encode32bitNumber(0), "yyyyyyyyyyyy");
-      assert.equal(zbase32.encode32bitNumber(23), "yyyyyyyyyyyz");
-      assert.equal(zbase32.encode32bitNumber(0x7fffffff), "yye9dhxt68a9");
-      assert.equal(zbase32.encode32bitNumber(0xffff), "yyyyyyyt68a9");
-    });
-
-    test("decode number", function() {
-      assert.equal(zbase32.decode32bitNumber("yyyyyyyyyyyy"), 0);
-      assert.equal(zbase32.decode32bitNumber("yyyyyyyyyyyz"), 23);
-      assert.equal(zbase32.decode32bitNumber("yye9dhxt68a9"), 0x7fffffff);
-      assert.equal(zbase32.decode32bitNumber("yyyyyyyt68a9"), 0xffff);
-    });
-
-    test("number roundtrip", function() {
-      const roundTrip = jsverify.forall("string", testString => {
-        const encoded = zbase32.encode(toUTF8(testString));
-        return testString === fromUTF8(zbase32.decode(encoded));
-      });
-
-      // FIXME: size is the max size of each generated random value,
-      // it doesn't increase the test string size significantly.
-      // See also:
-      // - https://github.com/jsverify/jsverify/issues/167
-      // - https://github.com/jsverify/jsverify/issues/169
-      jsverify.assert(roundTrip, { size: Number.MAX_SAFE_INTEGER });
-    });
-  });
 });
 
 // -*- mode: javascript-mode -*-
